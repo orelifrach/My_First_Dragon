b722cbaea7850f9cc1cd4f8869c81a5031eee24e
Or-el Ifrach <orel.ifrach17@gmail.com>
2026-03-03 12:11:52 +0200

    edit app.py

--- app.py
+++ app.py
@@ -15,19 +15,24 @@ Enter the pet type from the list
 Enter the pet's name: """)
         
         self.pet = Pet(name, type)
+        self.play()
         
     def play(self):
         actions_list = Pet.get_actions_list()
         actions_str = ""
         for i in range(len(actions_list)):
-            actions_str += f"{i+1} - {actions_list[i]-1}\n"
+            actions_str += f"{i+1} - {actions_list[i-1]}\n"
         request_str = "Enter the number of the requested action:\n" + actions_str
         requested_action = int(input(request_str))
+        while not isdigit(requested_action):
+            
         while not 0 < requested_action <= len(actions_list):
-            requested_action = int(input(f"The number of the requested number was out of range\n{request_str}"))
+            requested_action = int(input(f"The number of the requested action was out of range\n{request_str}"))
         
-        while True:
-            print
+        # while True:
+        #     if requested_action == 1:
+        #     elif requested_action == 2:
+        #     elif requested_action == 3:
 ##להמשיך מפה לממש את ממשק המשחק למשתמש
 
 app()
