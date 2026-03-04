dbf91ad6f7bbcf4e41ee1d1072c4fb2fffbaad7f
Or-el Ifrach <orel.ifrach17@gmail.com>
2026-03-04 10:17:51 +0200

    finished project!

--- app.py
+++ app.py
@@ -1,58 +1,68 @@
 from pet import Pet
 
-class app():
+
+class app:
     def __init__(self):
         type = input(f"""Enter the pet type from the list
-{Pet.get_pet_type_list()}: """)
-        while not Pet.verify_type(type):
+{Pet.get_pet_type_list(Pet)}: """)
+        while not Pet.verify_type(Pet, type):
             type = input(f"""The requested type is not found in the list,
-Enter the pet type from the list
-{Pet.get_pet_type_list()}: """)
-    
+                Enter the pet type from the list
+                {Pet.get_pet_type_list(Pet)}: """)
+
         name = input("Enter the pet's name: ")
-        while not Pet.verify_name(name):
-            name = input("""The name must be 2-9 chars long and contain only letters,
-Enter the pet's name: """)
-        
+        while not Pet.verify_name(Pet, name):
+            name = input(
+                "The name must be 2-9 chars long and contain only letters,"
+                "Enter the pet's name: "
+            )
+
         self._pet = Pet(name, type)
         self.start_game()
-        
+
     def input_action(self):
-        actions_list = Pet.get_actions_list() + ["exit"]
-        print(actions_list)
+        actions_list = Pet.get_actions_list(Pet) + ["exit"]
         actions_str = ""
         for i in range(len(actions_list)):
             actions_str += f"{i+1} - {actions_list[i]}\n"
-        output_str = "Enter the number of the requested action:\n" + actions_str
+        output_str = (
+            "Enter the number of the requested action:\n"
+            f"{actions_str}"
+        )
         # Check if the input is a digit and if the digit is valid
         requested_action_str = ""
         is_valid = False
         while not is_valid:
             while not requested_action_str.isdigit():
-                requested_action_str = input(f"The number of the requested action has to be a digit fronmt he options,\n{output_str}")
-            print(int(requested_action_str))
+                requested_action_str = input(
+                    "The number of the requested action has to be a digit "
+                    f"fronmt he options,\n{output_str}"
+                )
             if not 0 < int(requested_action_str) <= len(actions_list):
                 is_valid = False
-                requested_action_str = input(f"The number of the requested action has to be a digit from the options,\n{output_str}")
+                requested_action_str = input(
+                    "The number of the requested action has to be a digit "
+                    f"from the options,\n{output_str}"
+                )
             else:
                 is_valid = True
         return int(requested_action_str)
-        
+
     def start_game(self):
-        requested_action_num = self.input_action()      
-        while requested_action_num != 6: # exit
-            if requested_action_num == 1: # eat
+        requested_action_num = self.input_action()
+        while requested_action_num != 6:  # exit
+            if requested_action_num == 1:  # eat
                 self._pet.eat()
-            elif requested_action_num == 2: # sleep
+            elif requested_action_num == 2:  # sleep
                 self._pet.sleep()
-            elif requested_action_num == 3: # play
+            elif requested_action_num == 3:  # play
                 self._pet.play()
-            elif requested_action_num == 4: # state
+            elif requested_action_num == 4:  # state
                 self._pet.state()
-            elif requested_action_num == 5: # dascription
+            elif requested_action_num == 5:  # dascription
                 print(self._pet)
             requested_action_num = self.input_action()
-        print("The game is ended!")      
+        print("The game is over!")
 
 
 if __name__ == "__main__":
--- pet.py
+++ pet.py
@@ -2,50 +2,50 @@ from os import path
 from range_error import RangeError
 import logging
 
-class Pet():    
+
+class Pet:
     # Initial state
     HUNGER = 50
     HAPPINESS = 70
     ENERGY = 70
     POINTS = 10
-    
+
     # Default changes
     HUNGER_AFTER_EAT = -15
     HAPPINESS_AFTER_EAT = 5
     ENERGY_AFTER_EAT = 10
     POINTS_AFTER_EAT = -3
-    
+
     HUNGER_AFTER_SLEEP = 15
     ENERGY_AFTER_SLEEP = 20
-    
+
     HUNGER_AFTER_PLAY = 10
     HAPPINESS_AFTER_PLAY = 15
     ENERGY_AFTER_PLAY = -10
     POINTS_AFTER_PLAY = 2
-    
+
     # State Calculation
     HUNGER_PERCENT = 0.4
     HAPPINESS_PERCENT = 0.6
     ENERGY_PERCENT = 0.4
-    
+
     # Actions list
     _actions_list = ["eat", "sleep", "play", "state", "dascription"]
     # Pet type list
     _pet_type_list = ["dog", "cat", "fish"]
-    
-    def get_actions_list():
+
+    def get_actions_list(Pet):
         return Pet._actions_list
-    
-    def get_pet_type_list():
+
+    def get_pet_type_list(Pet):
         return Pet._pet_type_list
-    
-    def verify_type(type: str):
+
+    def verify_type(Pet, type: str):
         return type in Pet._pet_type_list
-    
-    def verify_name(name: str):
+
+    def verify_name(Pet, name: str):
         return 2 <= len(name) <= 9 and name.isalpha()
-    
-    
+
     def __init__(self, name: str, type: str):
         self._name = name
         self._type = type
@@ -53,71 +53,75 @@ class Pet():
         self._happiness = Pet.HAPPINESS
         self._energy = Pet.ENERGY
         self._points = Pet.POINTS
-        self._log_path = path.join(path.abspath(path.dirname("pet.py")), "history_log")
+        abs_path = path.abspath(path.dirname("pet.py"))
+        self._log_path = path.join(abs_path, "history_log")
 
     def eat(self):
         try:
             self._check_field("points", Pet.POINTS_AFTER_EAT)
             self._check_field("hunger", Pet.HUNGER_AFTER_EAT)
             self._check_field("energy", Pet.ENERGY_AFTER_EAT)
-            
+
             self._change_field("points", Pet.POINTS_AFTER_EAT)
             self._change_field("hunger", Pet.HUNGER_AFTER_EAT)
             self._change_field("energy", Pet.ENERGY_AFTER_EAT)
             self._change_field("happiness", Pet.HAPPINESS_AFTER_EAT)
-            
+
             msg = f"{self._name} has eaten"
             msg_status = "success"
 
         except RangeError as err:
             msg = f"Action eat was not made | {err}"
             msg_status = "error"
-        
+
         self._write_history(msg_status, msg)
         print(msg)
-    
-    
+
     def sleep(self):
         try:
             self._check_field("hunger", Pet.HUNGER_AFTER_SLEEP)
             self._check_field("energy", Pet.ENERGY_AFTER_SLEEP)
-            
+
             self._change_field("hunger", Pet.HUNGER_AFTER_SLEEP)
             self._change_field("energy", Pet.ENERGY_AFTER_SLEEP)
-            
+
             msg = f"{self._name} has slept"
             msg_status = "success"
-        
+
         except RangeError as err:
             msg = f"Action sleep was not made | {err}"
             msg_status = "error"
-        
+
         self._write_history(msg_status, msg)
         print(msg)
-            
+
     def play(self):
         try:
             self._check_field("points", Pet.POINTS_AFTER_PLAY)
             self._check_field("hungry", Pet.HUNGER_AFTER_PLAY)
             self._check_field("energy", Pet.ENERGY_AFTER_PLAY)
-            
+
             self._change_field("points", Pet.POINTS_AFTER_PLAY)
             self._change_field("hungry", Pet.HUNGER_AFTER_PLAY)
             self._change_field("energy", Pet.ENERGY_AFTER_PLAY)
             self._change_field("happiness", Pet.HAPPINESS_AFTER_PLAY)
-            
+
             msg = f"{self._name} has palyed"
             msg_status = "success"
-        
+
         except RangeError as err:
             msg = f"Action play was not made | {err}"
             msg_status = "error"
-        
+
         self._write_history(msg_status, msg)
         print(msg)
 
-    def state(self) -> float:
-        state = self._happiness * Pet.HAPPINESS_PERCENT + self._energy * Pet.ENERGY_PERCENT - self._hunger * Pet.HUNGER_PERCENT
+    def state(self) -> None:
+        state = (
+            self._happiness * Pet.HAPPINESS_PERCENT
+            + self._energy * Pet.ENERGY_PERCENT
+            - self._hunger * Pet.HUNGER_PERCENT
+        )
         state_str = f"The state of {self._name} the {self._type} is {state}"
         print(state_str)
 
@@ -133,8 +137,7 @@ energy level: {self._energy}
 points: {self._points}
 ------------------------------"""
         return description
-    
-    
+
     def _check_field(self, field: str, change: int):
         if field == "hunger":
             if self._hunger + change > 100:
@@ -143,13 +146,16 @@ points: {self._points}
                 raise RangeError("Your pet is too full to make this action")
         elif field == "energy":
             if self._energy + change < 0:
-                raise RangeError("Your pet doesnt have enough energy to make this action")
+                raise RangeError(
+                    "Your pet doesnt have enough energy to make this action"
+                )
         elif field == "points":
             if self._points + change < 0:
-                raise RangeError("You dont have enough points to make this action")
-    
+                raise RangeError(
+                    "You dont have enough points to make this action"
+                )
+
     def _change_field(self, field: str, change: int):
-        err_msg = ""
         if field == "hunger":
             self._hunger += change
         elif field == "happiness":
@@ -168,12 +174,17 @@ points: {self._points}
             self._points += change
 
     def _write_history(self, msg_status, msg):
-        logging.basicConfig(filename=self._log_path, format="%(asctime)s  %(levelname)s | %(message)s", datefmt="%m/%d/%Y %I:%M %p", level=logging.DEBUG)
+        logging.basicConfig(
+            filename=self._log_path,
+            format="%(asctime)s  %(levelname)s | %(message)s",
+            datefmt="%m/%d/%Y %I:%M %p",
+            level=logging.DEBUG,
+        )
         if msg_status == "success":
             logging.info(msg)
         elif msg_status == "error":
             logging.error(msg)
-        
+
 
 # p = Pet("puppy", "dog")
 # p.eat()
