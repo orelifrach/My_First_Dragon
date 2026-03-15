a754028e30f2ad318120206221398e21f94b6304
Or-el Ifrach <orel.ifrach17@gmail.com>
2026-03-15 17:50:58 +0200

    added the convert drom repr to an object

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, session
 from flask_session import Session
 from pet import Pet
 from database import database
-import requests
+import ctypes
 
 app = Flask("My First Dragon")
 db = database()
@@ -19,9 +19,9 @@ def test():
         password = request.form["password"]
         print(username, password)
         pet = db.get_pet(username, password)
-        return redirect("/start?pet_name=puppy&pet_type=dog")
+        # return redirect("/start?pet_name=puppy&pet_type=dog")
         # need to understand how to send the instance
-        # return redirect(f"/start?pet={pet}")
+        return redirect(f"/start?pet={pet}")
 
 @app.get("/start")
 def start():
@@ -29,25 +29,32 @@ def start():
     for i in request.args.values():
         counter += 1
     print(counter)
+    pet = None
     if counter > 1:
         pet_type = request.args["pet_type"]
         pet_name = request.args["pet_name"]
         # global pet
         pet = Pet(pet_name, pet_type)
     else:
-        # need to convert the print of the instance to the instance
-        pet = object(request.args["pet"])
-    return render_template("home.html")
+        pet_repr = request.args.get("pet")
+        pet = repr_to_pet(pet_repr)
+    # return render_template("home.html")
+    return redirect(f"/home?pet={pet}")
 
+@app.get("/home")
+def home():
+    pet_repr = request.args.get("pet")
+    return render_template("home.html", pet_repr=pet_repr)
 
 @app.get("/status")
 def get_status():
     return pet.get_status()
 
-
 @app.get("/show")
 def get_data():
     data = request.args.get("data")
+    pet_repr = request.args.get("pet_repr")
+    pet = repr_to_pet(pet_repr)
     if data == "hunger":
         return pet.get_hunger()
     if data == "happiness":
@@ -74,6 +81,11 @@ def sleep():
 def play():
     return pet.play()
 
+def repr_to_pet(pet_repr):
+    pet_hex = (pet_repr.split(" ")[-1][:-1])
+    pet_id = int(pet_hex, 16)
+    pet = ctypes.cast(pet_id, ctypes.py_object).value
+    return pet
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
